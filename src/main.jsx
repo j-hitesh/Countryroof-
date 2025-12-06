752287a1ce27c72a3b34f7d9db1a65c974e7b767
Hitesh Jangid <137302212+j-hitesh@users.noreply.github.com>
2025-12-06 17:17:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import React from 'react'
 import { PropertyProvider } from './Context/PropertyContext.jsx'
 
 createRoot(document.getElementById('root')).render(
