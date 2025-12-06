f9dd1f5fb03705e800d7cd0a711d43f33323e91b
Hitesh Jangid <137302212+j-hitesh@users.noreply.github.com>
2025-12-06 17:19:04 +0530

    Fix import path for PropertyProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { PropertyProvider } from './Context/PropertyContext.jsx'
+import { PropertyProvider } from './context/PropertyContext.jsx'
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
